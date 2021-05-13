ad79137a9b0bb6e4d4cd57e5e55b6d9c49004db6
Enrique Sarabia Roque <54542442+EnriqueSaro@users.noreply.github.com>
2021-05-13 12:19:11 -0500

    Add ranges filter to samples

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,71 +6,70 @@ const router = Router();
 
 router.get("/", (req,res) => {
     
-    let showAllData = (req.query.data) ? true : false;
+    const showAllData = (req.query.data === 'true') ? true : false;
     console.log(showAllData);
     
     const token = req.headers['x-request-id'];
     const url = process.env.SAMPLES_URL;
     
-    let nodes = JSON.parse(fs.readFileSync(url + '/nodes-description.json'));
-    let folder = nodes.filter(node => node.token === token)[0];
-    folder = folder.module_id;
+    let nodes = JSON.parse( fs.readFileSync( path.join(url,'nodes-description.json') ) );
+    let node_index = nodes.findIndex(node => node.token === token);
+    let folder = nodes[node_index].module_id;
+    let ranges = nodes[node_index].ranges;
 
-        // Read users.json file     
-    day = fs.readFileSync(url+folder+'/day.json');
-    yesterday = fs.readFileSync(url+folder+'/yesterday.json');        
-        // Converting to JSON 
-    let day_samples =JSON.parse(day); 
-    let yesterday_samples = JSON.parse(yesterday);
+    // Read samples from  files     
+    let day_samples = JSON.parse( fs.readFileSync( path.join(url,folder,'day.json') ) );
+    let yesterday_samples = JSON.parse( fs.readFileSync( path.join(url,folder,'yesterday.json') ) );        
 
-    if (day_samples.length == 0) {
-        res.status(404).send('Couldn\'t get response');
-    }        
-
+    let all_samples = [...yesterday_samples,...day_samples];
     let samples;
-    if(yesterday_samples.length >= 1399){
-        samples = [...yesterday_samples.slice(day_samples.length),...day_samples];
+    if ( ranges.length === 0){
+        samples = all_samples;
     }else{
-        samples = [...yesterday_samples,...day_samples];
+        samples = all_samples.filter( sample => {
+            let sample_hour = new Date(sample.fecha).getHours();
+            let is_in_range =  true;
+            for (let range of ranges) {
+                is_in_range &= (range.init < range.final) ?
+                    ( range.init <= sample_hour && sample_hour <= range.final) :
+                    ( range.init <= sample_hour || sample_hour <= range.final)
+            }
+            return is_in_range;
+        });
     }
+    console.log('inicio: ' + samples.length);
 
     if (samples.length > 1440){
         let index_needed = samples.length - 1440;
         samples = samples.slice(index_needed);
     }
+    console.log('fin: ' + samples.length);
 
     let last_sample = samples[samples.length -1];
     //Reverse samples because of the frontend need
     let reverse_samples = samples.reverse();
 
+    let data = {
+        voltage:  parseFloat(last_sample['voltaje'].toFixed(3)),
+        current:  parseFloat((last_sample['corriente'] * 1000).toFixed(3)),
+        production: parseFloat((last_sample['potencia_aparente']  / 60000 ).toFixed(7)),
+        aparentPower:  parseFloat(last_sample['potencia_aparente'].toFixed(3)),
+        activePower:  parseFloat(last_sample['potencia_activa'].toFixed(3)),
+        powerFactor:  parseFloat(last_sample['factor_potencia'].toFixed(3)),
+        frequency:  parseFloat(last_sample['frecuencia'].toFixed(3)),
+        quadrant: last_sample['cudrante'],
+    }
+
     if(showAllData){
-        res.status(200).json({
-            voltage:  parseFloat(last_sample['voltaje'].toFixed(3)),
-            current:  parseFloat(last_sample['corriente'].toFixed(3)),
-            production: parseFloat((last_sample['potencia_aparente'] * 60 / 1000).toFixed(3)),
-            aparentPower:  parseFloat(last_sample['potencia_aparente'].toFixed(3)),
-            activePower:  parseFloat(last_sample['potencia_activa'].toFixed(3)),
-            powerFactor:  parseFloat(last_sample['factor_potencia'].toFixed(3)),
-            frequency:  parseFloat(last_sample['frecuencia'].toFixed(3)),
-            quadrant: last_sample['cudrante'],
-            productionData: reverse_samples.map( function(sample) {
-                return {
-                    date: sample['fecha'],
-                    value: parseFloat((sample['potencia_aparente'] / 60000).toFixed(2))
-                }            
-            })
+        data.productionData = reverse_samples.map( function(sample) {
+            return {
+                date: sample['fecha'],
+                value: parseFloat((sample['potencia_aparente'] / 60000).toFixed(7))
+            }            
         });
+        res.status(200).json(data);
     }else{
-        res.status(200).json({
-            voltage:  parseFloat(last_sample['voltaje'].toFixed(3)),
-            current:  parseFloat(last_sample['corriente'].toFixed(3)),
-            production: parseFloat((last_sample['potencia_aparente']  / 60000).toFixed(2)),
-            aparentPower:  parseFloat(last_sample['potencia_aparente'].toFixed(3)),
-            activePower:  parseFloat(last_sample['potencia_activa'].toFixed(3)),
-            powerFactor:  parseFloat(last_sample['factor_potencia'].toFixed(3)),
-            frequency:  parseFloat(last_sample['frecuencia'].toFixed(3)),
-            quadrant: last_sample['cudrante'],
-        });
+        res.status(200).json(data);
     }
             
 });
